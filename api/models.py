aeb7288d5d0d0138f24b8383146cf67ff280a7e6
Tiago Fernandes <vsctiago7@gmail.com>
2020-01-26 01:09:26 +0000

    Adding Task model - WIP

--- api/models.py
+++ api/models.py
@@ -9,3 +9,9 @@ class Hero(models.Model):
 
     def __str__(self):
         return self.name
+
+class Task(models.Model):
+    """Task model"""
+    content = models.CharField(max_length=60)
+    isComplete = models.BooleanField()
+    createdAt = models.DateTimeField()
